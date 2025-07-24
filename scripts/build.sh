9037f7190f3168022c4d386fce66e7d9cd2300a9
Rath Pascal <rath@oxl.at>
2025-07-24 12:33:24 +0200

    update build-script

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,13 +8,19 @@ then
   exit 1
 fi
 
-echo "Building Version ${1}"
+VERSION="$1"
+echo "Building Version ${VERSION}"
 
 cd "$(dirname "$0")/.."
 
 rm -rf dist/*
 
-echo "$1" > VERSION
+echo "$VERSION" > VERSION
 python3 -m pip install -r ./requirements_build.txt >/dev/null
 python3 -m build
+if ls "dist/dnsbl-check-${VERSION}.tar.gz" >/dev/null
+then
+  mv "dist/dnsbl-check-${VERSION}.tar.gz" "dist/dnsbl_check-${VERSION}.tar.gz"
+fi
+
 # python3 -m twine upload --repository pypi dist/*
